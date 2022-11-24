9fc360069d91b64174de781ec805ec0f1232e2c6
Yen Mai <yenmai478@gmail.com>
2022-11-24 17:31:46 +0700

    Create infrastructure with docker-compose and init auth service

---
+++ authservice/src/test/java/me/yenmai/authservice/AuthserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yenmai.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
